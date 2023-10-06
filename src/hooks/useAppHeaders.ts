441ee86667ce3165268bfd0f9626cf4416df1662
Ian Mugambi <linksian63@gmail.com>
2023-10-06 09:43:34 +0300

    fix: theme headers

--- src/hooks/useAppHeaders.ts
+++ src/hooks/useAppHeaders.ts
@@ -2,10 +2,11 @@ import { cookies, headers } from 'next/headers';
 
 export function useAppHeaders() {
   const cookie = cookies();
-  const headersList = headers();
-  const pathName = headersList.get('path-name');
-  const hideHeader = headersList.get('hide-header');
-  const darkMode =
-    !cookie.get('theme') || cookie.get('theme')?.value === 'dark';
+  const header = headers();
+  const pathName = header.get('path-name');
+  const hideHeader = header.get('hide-header');
+  const darkMode = !cookie.get('theme')
+    ? header.get('theme') === 'dark'
+    : cookie.get('theme')?.value === 'dark';
   return { darkMode, pathName, hideHeader };
 }
